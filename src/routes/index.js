14baca5e5b2da43c65bea5666db588c57388fd2b
Jumperenterprise <53949770+Jumperenterprise@users.noreply.github.com>
2020-05-06 15:19:21 -0600

    Deploying

---
+++ src/routes/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const passport = require('passport');
+
+router.get('/', (req, res,  next) => {
+    res.render('index');
+
+});
+
+router.get('/signup', (req, res, next) => {
+    res.render('signup');
+
+
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    passReqToCallback: true
+    
+}));
+
+router.get('/signin', (req, res, next) => {
+    res.render('signin');
+  });
+  
+  
+  router.post('/signin', passport.authenticate('local-signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+    failureFlash: true
+  }));
+  
+  router.get('/profile',isAuthenticated, (req, res, next) => {
+    res.render('profile');
+  });
+
+router.get('/logout', (req, res, next) => {
+    req.logOut();
+    res.redirect('/');
+});
+
+router.use((req, res, next) => {
+    isAuthenticated(req, res, next);
+    next();
+});
+
+router.get('/profile', (req, res, next) => {
+    res.render('profile');
+});
+
+function isAuthenticated(req, res, next)  {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+};
+
+module.exports = router;
